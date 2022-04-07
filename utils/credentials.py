ca81e5cee2cda556310d58822d13709b918c3bd9
Arie White <arie.white@goodleap.com>
2022-04-07 08:57:34 -0700

    resolving review comments

---
+++ utils/credentials.py
@@ -0,0 +1,3 @@
+# credentials for sending opt-out response 
+basicAuthUser = '06aae2ee-2d7e-41c9-87fd-604779c9cdc2'
+basicAuthPassword = 'd2364738-8075-4101-a22c-e4847d99781e'
--- utils/variables.py
+++ utils/variables.py
@@ -7,3 +7,7 @@ FAILED_CAPTCHA_DOWNLOAD_FOLDER = ROOT_DIR + "/failed_captchas/"
 UTILS_FOLDER = ROOT_DIR + "/utils/"
 DATA_MODEL_FOLDER = UTILS_FOLDER + "/data_models/"
 JSON_DOWNLOAD_BASE64 = FAILED_CAPTCHA_DOWNLOAD_FOLDER + "failed_captchas_base64.json"
+
+# url variables
+API_ROOT_URL = "https://lrdcnxcfc3.execute-api.us-west-2.amazonaws.com"
+API_RESULTS_URL = API_ROOT_URL + "/result"
