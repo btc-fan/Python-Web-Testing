388e158b7bfa3063333800323166665d1ac1a4a6
Arie White <arie.white@goodleap.com>
2022-04-07 10:45:18 -0700

    adding logic to read response objects from file

--- utils/send_opt_request.py
+++ utils/send_opt_request.py
@@ -8,51 +8,29 @@ authUser = creds.basicAuthUser
 authPass = creds.basicAuthPassword
 
 # verify the user action is authorized, should return 200 status
-
-
 def check_connection():
     r = requests.get(variables.API_ROOT_URL, auth=(authUser, authPass))
     print(r.status_code)
     print(r.text)
 
-# returns a json formatted payload for submission
+# returns a json formatted payload for POST operation
+def prepare_payload():
+    response_file = variables.GENERATED_JSONS
+    with open(response_file, 'r') as responseFile:
+        data = json.load(responseFile)
+        for obj in data:
+            send_request(obj)
 
-
-def get_payload():
-    request_payload = {
-        "UUID": "<%- || null %>",
-        "submitSuccess": "<%- || false %>",
-        "attemptAt": "<%- || null %>",
-        "completedAt": "<%- || null %>",
-        "attempts": "<%- || 0 %>",
-        "logs": "<%- || null %>",
-        "captcha": [
-            {
-                "success": "<%- || true %>",
-                "img": "<%- 'base64 encoded image' || null %>"
-            }
-        ],
-        "responseMessage": "<%- 'string' || null %>",
-        "errors": []
-    }
-    return request_payload
-
-# form request to POST
-
-
-def send_request():
-    requestPayload = get_payload()
-    data = json.dumps(requestPayload)
+# POST request 
+def send_request(data):
     r = requests.post(variables.API_RESULTS_URL, auth=(
         authUser, authPass), json=data)
     print(r.status_code)
     print(r.text)
 
-
 def main():
     check_connection()
-    send_request()
-
+    prepare_payload()
 
 if __name__ == '__main__':
     sys.exit(main())
